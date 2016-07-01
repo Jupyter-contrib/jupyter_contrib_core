92865ca891722c87c04b3913d5fd02b4fe47a8c5
Josh Barnes <joshuacookebarnes@gmail.com>
2016-07-01 18:32:22 +0100

    move raise_on_bad_version into testing_utils

--- src/jupyter_contrib_core/testing_utils/__init__.py
+++ src/jupyter_contrib_core/testing_utils/__init__.py
@@ -7,11 +7,33 @@ from __future__ import (
 
 import logging
 import os
+import re
 import sys
 from threading import RLock
 
 from traitlets.config.application import LevelFormatter
 from traitlets.traitlets import default
+
+try:
+    from notebook.tests.test_notebookapp import raise_on_bad_version
+except ImportError:
+    pep440re = re.compile(
+        r'^'
+        r'([1-9]\d*!)?(0|[1-9]\d*)(.(0|[1-9]\d*))*'
+        r'((a|b|rc)(0|[1-9]\d*))?'
+        r'(\.post(0|[1-9]\d*))?'
+        r'(\.dev(0|[1-9]\d*))?'
+        r'$'
+    )
+
+    def raise_on_bad_version(version):
+        if not pep440re.match(version):
+            raise ValueError(
+                "Versions String apparently does not match Pep 440 "
+                "specification, which might lead to sdist and wheel being "
+                "seen as 2 different release. "
+                "E.g: do not use dots for beta/alpha/rc markers."
+            )
 
 
 def stringify_env(env):
