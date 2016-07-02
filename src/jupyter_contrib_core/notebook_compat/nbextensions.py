373ee7d1f8d6a7d2344a3caf9ecef4eaa1405aea
Josh Barnes <joshuacookebarnes@gmail.com>
2016-07-02 11:20:37 +0100

    include almost-full 4.2 nbextensions public API

--- src/jupyter_contrib_core/notebook_compat/nbextensions.py
+++ src/jupyter_contrib_core/notebook_compat/nbextensions.py
@@ -3,13 +3,52 @@
 
 try:
     from notebook.nbextensions import (
-        GREEN_OK, RED_X, BaseNBExtensionApp, _get_config_dir,
+        # constants
+        GREEN_ENABLED, GREEN_OK, NBCONFIG_SECTIONS, RED_DISABLED, RED_X,
+        # Apps & classes
+        ArgumentConflict, BaseNBExtensionApp,
+        # public API functions
+        _set_nbextension_state, _set_nbextension_state_python,
+        disable_nbextension, disable_nbextension_python,
+        enable_nbextension, enable_nbextension_python,
+        install_nbextension, install_nbextension_python,
+        uninstall_nbextension, uninstall_nbextension_python,
+        validate_nbextension, validate_nbextension_python,
+        # private API functions
+        _get_nbextension_dir, _nbextension_dirs, _get_config_dir,
+        _get_nbextension_metadata,
     )
 except ImportError:
     from ._compat.nbextensions import (
-        GREEN_OK, RED_X, BaseNBExtensionApp, _get_config_dir,
+        # constants
+        GREEN_ENABLED, GREEN_OK, NBCONFIG_SECTIONS, RED_DISABLED, RED_X,
+        # Apps & classes
+        ArgumentConflict, BaseNBExtensionApp,
+        # public API functions
+        _set_nbextension_state, _set_nbextension_state_python,
+        disable_nbextension, disable_nbextension_python,
+        enable_nbextension, enable_nbextension_python,
+        install_nbextension, install_nbextension_python,
+        uninstall_nbextension, uninstall_nbextension_python,
+        validate_nbextension, validate_nbextension_python,
+        # private API functions
+        _get_nbextension_dir, _nbextension_dirs, _get_config_dir,
+        _get_nbextension_metadata,
     )
 
 __all__ = [
-    'GREEN_OK', 'RED_X', 'BaseNBExtensionApp', '_get_config_dir',
+    # constants
+    'GREEN_ENABLED', 'GREEN_OK', 'NBCONFIG_SECTIONS', 'RED_DISABLED', 'RED_X',
+    # Apps & classes
+    'ArgumentConflict', 'BaseNBExtensionApp',
+    # public API functions
+    '_set_nbextension_state', '_set_nbextension_state_python',
+    'disable_nbextension', 'disable_nbextension_python',
+    'enable_nbextension', 'enable_nbextension_python',
+    'install_nbextension', 'install_nbextension_python',
+    'uninstall_nbextension', 'uninstall_nbextension_python',
+    'validate_nbextension', 'validate_nbextension_python',
+    # private API functions
+    '_get_nbextension_dir', '_nbextension_dirs', '_get_config_dir',
+    '_get_nbextension_metadata',
 ]
