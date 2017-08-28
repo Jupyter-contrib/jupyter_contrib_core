69eea83e1d82c9af1d53ff21a0178e90cf4d6dbd
Josh Barnes <joshuacookebarnes@gmail.com>
2017-08-28 10:36:50 +0000

    release 0.3.2

--- setup.py
+++ setup.py
@@ -23,14 +23,14 @@ Common utilities for jupyter-contrib projects. Includes:
 -   common application components and cli scripts
 -   utility classes and functions for use in tests
 """,
-        version='0.3.1',
+        version='0.3.2',
         author='jcb91, jupyter-contrib developers',
         author_email='joshuacookebarnes@gmail.com',
         url=('https://github.com/'
              'jupyter-contrib/jupyter_contrib_core'),
         download_url=('https://github.com/'
                       'jupyter-contrib/jupyter_contrib_core/'
-                      'tarball/0.3.1'),
+                      'tarball/0.3.2'),
         keywords=['Jupyter', 'notebook'],
         license='BSD 3-clause',
         platforms=['any'],
--- src/jupyter_contrib_core/__init__.py
+++ src/jupyter_contrib_core/__init__.py
@@ -5,4 +5,4 @@
 
 from __future__ import unicode_literals
 
-__version__ = '0.3.1'
+__version__ = '0.3.2'
