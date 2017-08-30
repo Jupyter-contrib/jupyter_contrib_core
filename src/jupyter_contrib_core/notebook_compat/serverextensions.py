6d1eb799abe71b2e50489b870769a90977d7291d
Josh Barnes <joshuacookebarnes@gmail.com>
2017-08-31 00:58:15 +0100

    more updates to cope with changes for notebook 5.x

--- src/jupyter_contrib_core/notebook_compat/serverextensions.py
+++ src/jupyter_contrib_core/notebook_compat/serverextensions.py
@@ -1,22 +1,27 @@
 # -*- coding: utf-8 -*-
 """Shim providing notebook.serverextensions stuff for pre 4.2 versions."""
 
-try:  # notebook >= 4.2
+try:
+    # notebook >= 4.2
     from notebook.serverextensions import (
         ToggleServerExtensionApp, toggle_serverextension_python,
     )
-except ImportError:  # notebook <4.2
+except ImportError:
+    # notebook <4.2
     from ._compat.serverextensions import (
         ToggleServerExtensionApp, toggle_serverextension_python,
     )
 
 try:
-    from notebook.extensions import ArgumentConflict  # notebook >= 5.0
+    # notebook >= 5.0
+    from notebook.extensions import ArgumentConflict
 except ImportError:
     try:
-        from notebook.serverextensions import ArgumentConflict  # notebook 4.2.x
+        # notebook 4.2.x
+        from notebook.serverextensions import ArgumentConflict
     except ImportError:
-        from ._compat.serverextensions import ArgumentConflict  # notebook < 4.2
+        # notebook < 4.2
+        from ._compat.serverextensions import ArgumentConflict
 
 
 __all__ = [
